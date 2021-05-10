4f27c9df013e8e373b1530b3af2646ac8728cbc2
Mstislav Sergeev <m.n.sergeev@gmail.com>
2021-05-10 23:16:16 +0300

    [testing] PHPUnit install

---
+++ src/Tests/Creator/PosterTest.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace TelegramLogger\Tests\Creator;
+
+use PHPUnit\Framework\TestCase;
+
+/**
+ * Class PosterTest
+ * @package TelegramLogger\Tests\Creator
+ */
+class PosterTest extends TestCase
+{
+    public function testPost(): void
+    {
+
+    }
+}
